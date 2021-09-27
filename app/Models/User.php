9bc9022b04894ae1be63628af126047436359664
Christian Ramos <crismas06@outlook.com>
2021-09-27 08:21:13 -0500

    prueba 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    function adminlte_profile_url(){
+        return 'user/profile';
+    }
 }
